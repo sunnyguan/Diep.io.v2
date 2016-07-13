49ad24feed2bc97965609d6b988a1c881f45cde5
sunnyguan <sunny.peng.guan@gmail.com>
2016-07-13 12:57:18 -0700

    message

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 var serv = require('http').Server(app);
 
 app.get('/',function(req, res) {
-	res.sendFile(__dirname + './index.html');
+	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client',express.static(__dirname + '/client'));
 
